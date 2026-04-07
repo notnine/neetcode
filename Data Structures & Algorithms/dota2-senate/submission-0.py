35ba1deb9c5a6a3b958bb1dc236bd0c0c423316e
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-04-07 16:10:10 -0400

    Add: dota2-senate - submission-0

---
+++ Data Structures & Algorithms/dota2-senate/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        # approach 0: keep a list of banned senates where banned[i] = true if banned
+        banned = [False] * len(senate)
+        radiants = [] # list of the indices of unbanned radiants
+        dires = [] # list of the indices of unbanned dires
+
+        for i, s in enumerate(senate):
+            if s == 'R':
+                radiants.append(i)
+            else:
+                dires.append(i)
+        
+        radiants.reverse()
+        dires.reverse()
+        
+        i = 0 
+        while i < len(senate):
+            if not banned[i]:
+                # ban i's first opp
+                if senate[i] == 'R':
+                    dire = dires.pop()
+                    banned[dire] = True
+                    if len(dires) == 0:
+                        return "Radiant"
+                else:
+                    radiant = radiants.pop()
+                    banned[radiant] = True
+                    if len(radiants) == 0:
+                        return "Dire"
+            i += 1
+        
+        return "Radiant" if len(radiants) > 0 else "Dire"
