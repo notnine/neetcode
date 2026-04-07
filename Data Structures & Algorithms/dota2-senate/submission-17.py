98b2cccd3b1cef8889fa840b207ffbbc1a2d25f6
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-04-07 18:40:11 -0400

    Add: dota2-senate - submission-17

---
+++ Data Structures & Algorithms/dota2-senate/submission-17.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        # the key is that the curr senate needs to ban the next opp that hasn't gone
+
+        dires = deque()
+        radiants = deque()
+
+        for i, c in enumerate(senate):
+            if c == 'D':
+                dires.append(i)
+            else:
+                radiants.append(i)
+        
+
+        while dires and radiants:
+            print("Dires: " + str(dires))
+            print("Radiants: " + str(radiants))
+            d, r = dires.popleft(), radiants.popleft()
+            print("d, r: " + str([d, r]))
+            # if d goes first, then d bans r, else vice versa
+            if d < r:
+                print("appending " + str(d) + " to dires")
+                dires.append(d + len(senate)) # append n to signify that d's next turn is in the "next" iteration
+            else:
+                print("appending " + str(r) + " to radiants")
+                radiants.append(r + len(senate))
+            print()
+        
+        print("Final Dires: " + str(dires))
+        print("Final Radiants: " + str(radiants))
+        return 'Dire' if dires else 'Radiant'
