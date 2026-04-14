1131dec7c133ab4944018b9d2f05186300d445f6
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-04-14 07:53:30 -0400

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # graph traversal with relaxation
+        # relax neighbors, traverse to nei only if we found a cheaper way to nei
+
+
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        ROWS, COLS = len(grid), len(grid[0])
+        cost = [[float('inf') for c in range(COLS)] for r in range(ROWS)] # best cost to reach pos (r,c) so far
+        cost[0][0] = grid[0][0]
+
+        def relax(r: int, c: int) -> None:
+            for d_r, d_c in directions:
+                n_r, n_c = r + d_r, c + d_c
+                if 0 <= n_r < ROWS and 0 <= n_c < COLS and cost[n_r][n_c] > max(grid[n_r][n_c], cost[r][c]):
+                    cost[n_r][n_c] = max(grid[n_r][n_c], cost[r][c])
+                    relax(n_r,n_c)
+
+        relax(0,0)
+        return cost[ROWS - 1][COLS - 1]
