ebb2bb7f0589ebfc25ae45b8667a3a4f024120f0
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-04-25 16:46:08 -0400

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        ROWS, COLS = len(image), len(image[0])
+        ori_color = image[sr][sc]
+
+        curr = [(sr, sc)] if image[sr][sc] != color else []
+        image[sr][sc] = color
+
+        while curr:
+            r, c = curr.pop()
+            for d_r, d_c in directions:
+                n_r, n_c = r + d_r, c + d_c
+                if 0 <= n_r < ROWS and 0 <= n_c < COLS and image[n_r][n_c] == ori_color:
+                    curr.append((n_r, n_c))
+                    image[n_r][n_c] = color
+        
+        return image
