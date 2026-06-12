ebff3ec29e23aa1d3cf55d8e48f9bee977c66b8e
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-06-12 08:30:25 -0400

    Add: string-compression - submission-2

---
+++ Data Structures & Algorithms/string-compression/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        s = ''
+        curr_char = None
+        curr_count = 0
+
+        for c in chars:
+            if curr_char is None or c == curr_char:
+                curr_count += 1
+                curr_char = c
+            else:
+                if curr_count == 1:
+                    s += curr_char
+                else:
+                    s += curr_char
+                    s += str(curr_count)
+                curr_count = 0
+                curr_char = c
+        
+        # append the last
+        s += c
+        s = s + str(curr_count) if curr_count > 1 else s
+        print(s)
+        for i in range(len(s)):
+            print(i)
+            chars[i] = s[i]
+        
+        return len(s)
