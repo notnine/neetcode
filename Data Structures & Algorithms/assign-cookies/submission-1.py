094573c5db826009b0902eb09910e708228d46e2
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-08-27 17:41:13 -0400

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i = 0
+        res = 0
+
+        for child in g:
+            # while this cookie cannot satisfy this child, go to the next cookie
+            while i < len(s) and s[i] < child:
+                i += 1
+            # if this cookie can satisfy, increment res by 1
+            if i < len(s) and s[i] >= child:
+                res += 1
+                i += 1
+        
+        return res
