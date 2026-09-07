70e4bfc65ed68359dd04635f0e01c74c683e7e0e
Glen M. Jeremy <89920772+notnine@users.noreply.github.com>
2026-09-07 15:33:53 -0400

    Add: valid-perfect-square - submission-0

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        
+        # bin search
+        l, r = 0, num // 2
+
+        while l <= r:
+            m = (l + r) // 2
+            if m * m == num:
+                return True
+            elif m * m > num:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return False
